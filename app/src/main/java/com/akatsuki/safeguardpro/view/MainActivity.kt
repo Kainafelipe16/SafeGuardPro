d3d557c7edfbaf620431699532649704f2173023
Vinicius Henrque <vinicius.barbiere@portalsesisp.org.br>
2024-04-01 08:20:14 -0300

    criação da tela de entrega de epi

--- app/src/main/java/com/akatsuki/safeguardpro/view/MainActivity.kt
+++ app/src/main/java/com/akatsuki/safeguardpro/view/MainActivity.kt
@@ -2,11 +2,24 @@ package com.akatsuki.safeguardpro.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import com.akatsuki.safeguardpro.R
+import com.akatsuki.safeguardpro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding : ActivityMainBinding? = null
+    private val binding : ActivityMainBinding get()= _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(binding.navContainer.id) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        setContentView(binding.root)
     }
 }
