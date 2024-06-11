fae26dc4db521c83e38e574276f211d33ef9345a
Matheus01~ <matheusbarbosa2827@gmail.com>
2024-06-11 11:29:29 -0300

    Fragments de EPI

--- app/src/main/java/com/akatsuki/safeguardpro/service/repository/EpiRepository.kt
+++ app/src/main/java/com/akatsuki/safeguardpro/service/repository/EpiRepository.kt
@@ -3,7 +3,7 @@ package com.akatsuki.safeguardpro.service.repository
 import android.content.Context
 import com.akatsuki.safeguardpro.service.model.Epi
 import com.akatsuki.safeguardpro.service.repository.remote.EpiService
-import com.rafael.firstapp.service.repository.remote.RetrofitClient
+import com.akatsuki.safeguardpro.service.repository.remote.RetrofitClient
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.RequestBody.Companion.toRequestBody
 
